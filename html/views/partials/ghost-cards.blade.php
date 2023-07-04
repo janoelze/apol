8ebbe175466a41e31f8a5040d99c7d9d47adc407
Jan Oelze <janoelzelebt@googlemail.com>
2023-07-04 20:18:36 +0200

    Update ghost-cards.blade.php

--- html/views/partials/ghost-cards.blade.php
+++ html/views/partials/ghost-cards.blade.php
@@ -1,16 +1,13 @@
 <div class="ghost-cards">
     @for ($i = 0; $i < 10; $i++)
-        <div class="ghost-card tint-bg-down-2"></div>
+        <div class="ghost-card tint-bg-down-2" style="height: {{ 45 * rand(3,6) }}px;"></div>
     @endfor
     <style>
         .ghost-cards {
             display: flex;
             flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            height: 100%;
             width: 100%;
-            gap: 10px
+            gap: 5px;
         }
 
         @keyframes fade-in {
@@ -18,7 +15,7 @@
                 opacity: 0.1;
             }
             50% {
-                opacity: 0.6;
+                opacity: 0.85;
             }
             100% {
                 opacity: 0.1;
@@ -27,7 +24,6 @@
 
         .ghost-card {
             width: 100%;
-            min-height: 150px;
             animation: fade-in .87s infinite linear;
         }
     </style>
