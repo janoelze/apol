b6b6333e489436b53f01cd2a881449a65401562f
Lukas <hi@lukas.io>
2023-07-05 21:35:19 +0200

    remove link element on post card

--- html/views/partials/post.blade.php
+++ html/views/partials/post.blade.php
@@ -3,9 +3,9 @@
 @else
   @if($is_last)
     {{-- hx-get="{!! Helpers::get_next_page($data) !!}" hx-select=".list-t3" --}}
-    <a class="thing t3 tint-bg-down-2 {{ $data['over_18'] ? 'is-nsfw' : '' }}" href="{{ Helpers::get_base_url() }}{{ $data['permalink'] }}" hx-get="{!! Helpers::get_next_page($value['data']) !!}" hx-trigger="revealed" hx-select=".list-t3 .thing" hx-swap="afterend" hx-indicator=".loading-indicator">
+    <section class="thing t3 tint-bg-down-2 {{ $data['over_18'] ? 'is-nsfw' : '' }}" hx-on="click: window.location.href='{{ Helpers::get_base_url() }}{{ $data['permalink'] }}'" hx-get="{!! Helpers::get_next_page($value['data']) !!}" hx-trigger="revealed" hx-select=".list-t3 .thing" hx-swap="afterend" hx-indicator=".loading-indicator">
   @else
-    <a class="thing t3 tint-bg-down-2 {{ $data['over_18'] ? 'is-nsfw' : '' }}" href="{{ Helpers::get_base_url() }}{{ $data['permalink'] }}" >
+    <section class="thing t3 tint-bg-down-2 {{ $data['over_18'] ? 'is-nsfw' : '' }}" hx-on="click: window.location.href='{{ Helpers::get_base_url() }}{{ $data['permalink'] }}'" >
   @endif
 @endif
   <div class="title tint-fg-up-65 ">{{ $data['title'] }}</div>
@@ -18,7 +18,7 @@
     </div>
   @endif
   @if($data['url'] ?? false)
-    <div class="url tint-bg-up-2">
+    <a href="{{$data['url']}}" class="url tint-bg-up-2">
       <div class="url-icon tint-fg-up-35">{!! Helpers::embed('./img/link.svg') !!}</div>
       <div class="url-text tint-fg-up-35">
         <div>
@@ -26,11 +26,11 @@
         </div>
       </div>
       <div class="url-icon tint-fg-up-35">{!! Helpers::embed('./img/chevron-right.svg') !!}</div>
-    </div>
+    </a>
   @endif
   <div class="meta">
     <div class="tint-fg-up-35">{!! Helpers::embed('./img/arrow-up.svg') !!} {{ Helpers::formatk($data['ups']) }}</div>
     <div class="tint-fg-up-35">{!! Helpers::embed('./img/message-circle.svg') !!} {{ Helpers::formatk($data['num_comments']) }}</div>
     <div class="tint-fg-up-35">{!! Helpers::embed('./img/clock.svg') !!} {{ Helpers::relative_time($data['created_utc']) }}</div>
   </div>
-</a>
+</section>
