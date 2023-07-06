3ec139fb86f8603fd9d7a96778972508e07a7c8f
Lukas <hi@lukas.io>
2023-07-06 09:07:44 +0200

    save areas

--- html/views/layouts/app.blade.php
+++ html/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <meta name="apple-mobile-web-app-title" content="Apol">
     <meta name="theme-color" content="#121823">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"> 
     <style>
         {!! Helpers::embed('apol.css') !!} {!! Helpers::embed('tinting.css') !!}
     </style>
