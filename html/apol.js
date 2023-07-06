90b3e5634e3f038f576fea6c7356e23075febbd7
Lukas <hi@lukas.io>
2023-07-06 16:21:54 +0200

    Update apol.js

--- html/apol.js
+++ html/apol.js
@@ -22,8 +22,24 @@ function initPullToRefresh() {
     });
 }
 
+function initScrolltoTop() {
+    //remove all listeners from scroll to top
+    document.querySelector('.scroll-to-top').removeEventListener('click', function() {});
+
+    document.querySelector('.scroll-to-top').addEventListener('click', function() {
+        // scroll to top logic
+        //scroll the .content div to the top
+        document.querySelector('.container').scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+}
+
 function onLoad() {
     console.log("hello :^)");
+    initScrolltoTop();
 }
 
 window.addEventListener("DOMContentLoaded", onLoad);
